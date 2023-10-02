cbf85c88e3bd56b8e2c69a2d20e4e92a119bdd5e
Dineshpalappdeveloper <dineshkr748199@gmail.com>
2023-10-02 21:04:25 +0530

    faster

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const Data = require("./data");
 const app = express()
-const cors = require("cors")
+const cors = require("cors");
+const { Data, Hollywwood, Bollywood, Food, Tech, all } = require("./data");
 const port = 3005;
 
 app.use(cors({
@@ -16,6 +16,26 @@ app.get("/", (req, res) => {
 app.get("/bihar", (req, res) => {
     res.send(Data)
     console.log(Data);
+})
+app.get("/Hollywwood", (req, res) => {
+    res.send(Hollywwood)
+    console.log(Hollywwood);
+})
+app.get("/Bollywood", (req, res) => {
+    res.send(Bollywood)
+    console.log(Bollywood);
+})
+app.get("/Food", (req, res) => {
+    res.send(Food)
+    console.log(Food);
+})
+app.get("/Tech", (req, res) => {
+    res.send(Tech)
+    console.log(Tech);
+})
+app.get("/all", (req, res) => {
+    res.send(all)
+    console.log(all);
 })
 
 app.listen(port, () => {
